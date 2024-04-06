48070008811ec0030a239553b0b2901c412eb8cf
IMFINETHK <148548348+IMFINETHK@users.noreply.github.com>
2024-04-07 00:18:50 +0900

    Add files via upload

--- src/component/OrderEditor.jsx
+++ src/component/OrderEditor.jsx
@@ -1,32 +1,41 @@
 //Day11. 배달 음식 주문 페이지 만들기
-import { useState } from "react";
-const OrderEditor = () => {
-  const [menu, setMenu] = useState("");
-  const [address, setaddress] = useState("");
-  const [request, setRequest] = useState("");
+import { useState, useRef } from "react";
 
-  const onChangeMenun = (e) => {
-    setMenu(e.target.value);
+const OrderEditor = () => {
+  const [input, setInput] = useState({
+    menu: "",
+    address: "",
+    request: "",
+  });
+
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
-  const onChangeAddress = (e) => {
-    setaddress(e.target.value);
-  };
-  const onChangeRequest = (e) => {
-    setRequest(e.target.value);
-  };
+  const inputRef = useRef();
   const onSubmit = () => {
-    alert(
-      `주문이 완료되었습니다! 메뉴: ${menu}, 주소: ${address}, 요청사항: ${request}`
-    );
+    if (input.address === "") {
+      inputRef.current.focus();
+    } else {
+      alert(
+        `주문이 완료되었습니다! 메뉴: ${input.menu} 주소: ${input.address} 배달요청사항: ${input.request}`
+      );
+    }
   };
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
       <h2>배달의민족 주문</h2>
       <div>
-        <div value={menu} style={{ marginBottom: 5, fontSize: 14 }}>
+        <div
+          name="menu"
+          value={input.menu}
+          style={{ marginBottom: 5, fontSize: 14 }}
+        >
           메뉴 선택
         </div>
-        <select onChange={onChangeMenun} style={{ width: 300, padding: 5 }}>
+        <select onChange={onChange} style={{ width: 300, padding: 5 }}>
           <option value={"족발"}>족발</option>
           <option value={"떡볶이"}>떡볶이</option>
           <option value={"아이스크림"}>아이스크림</option>
@@ -36,8 +45,10 @@ const OrderEditor = () => {
       <div>
         <div style={{ marginBottom: 5, fontSize: 14 }}>배달 주소</div>
         <input
-          value={address}
-          onChange={onChangeAddress}
+          ref={inputRef}
+          name="address"
+          value={input.address}
+          onChange={onChange}
           style={{ width: 300, padding: 5 }}
           placeholder="주소) 서울특별시 xx동 .."
         />
@@ -45,8 +56,9 @@ const OrderEditor = () => {
       <div>
         <div style={{ marginBottom: 5, fontSize: 14 }}>배달 요청사항</div>
         <textarea
-          value={request}
-          onChange={onChangeRequest}
+          name="request"
+          value={input.request}
+          onChange={onChange}
           style={{ width: 300, padding: 5 }}
           placeholder="배달 요청사항을 써 주세요..."
         />
